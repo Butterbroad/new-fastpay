2a043a0bed21d83eb22cd8a6843a0cc58f66d48a
butterbroad <pyatochevskiy@gmail.com>
2020-10-13 18:14:57 +0300

    initial commit

---
+++ #src/js/script.js
@@ -0,0 +1,45 @@
+//get link with parameters
+const paramLinks = document.querySelectorAll('.get-link');
+if (paramLinks.length > 0) {
+  paramLinks.forEach(link => {
+    link.setAttribute('href', link.getAttribute('href') + window.location.search);
+  });
+}
+
+//modal
+const modalTrigger = document.querySelector('.hero__info');
+modalTrigger.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('active');
+
+  const modalClose = document.querySelector('.modal-info__close');
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+
+  const modalLink = document.querySelector('.modal-info__list-link');
+  modalLink.addEventListener('click', e => {
+    e.preventDefault();
+    modal.classList.remove('active');
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@@include('webpSupport.js')
---
+++ new-fastpay/js/script.js
@@ -0,0 +1,61 @@
+//get link with parameters
+const paramLinks = document.querySelectorAll('.get-link');
+if (paramLinks.length > 0) {
+  paramLinks.forEach(link => {
+    link.setAttribute('href', link.getAttribute('href') + window.location.search);
+  });
+}
+
+//modal
+const modalTrigger = document.querySelector('.hero__info');
+modalTrigger.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('active');
+
+  const modalClose = document.querySelector('.modal-info__close');
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+
+  const modalLink = document.querySelector('.modal-info__list-link');
+  modalLink.addEventListener('click', e => {
+    e.preventDefault();
+    modal.classList.remove('active');
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
