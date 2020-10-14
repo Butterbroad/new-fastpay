6fdb3bbf5c7a073e139bec3ee5671359c543d059
butterbroad <pyatochevskiy@gmail.com>
2020-10-14 17:56:33 +0300

    14.10.2020 17:56

--- new-fastpay/js/script.js
+++ new-fastpay/js/script.js
@@ -1,42 +1,29 @@
-//get link with parameters
-const paramLinks = document.querySelectorAll('.get-link');
-if (paramLinks.length > 0) {
-  paramLinks.forEach(link => {
-    link.setAttribute('href', link.getAttribute('href') + window.location.search);
+document.addEventListener('DOMContentLoaded', () => {
+  //get link with parameters
+  const paramLinks = document.querySelectorAll('.get-link');
+  if (paramLinks.length > 0) {
+    paramLinks.forEach(link => {
+      link.setAttribute('href', link.getAttribute('href') + window.location.search);
+    });
+  }
+
+  //modal
+  const modalTrigger = document.querySelector('.hero__info');
+  modalTrigger.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('active');
+
+    const modalClose = document.querySelector('.modal-info__close');
+    modalClose.addEventListener('click', () => {
+      modal.classList.remove('active');
+    });
   });
-}
-
-//modal
-const modalTrigger = document.querySelector('.hero__info');
-modalTrigger.addEventListener('click', () => {
-  const modal = document.querySelector('.modal');
-  modal.classList.add('active');
-
-  const modalClose = document.querySelector('.modal-info__close');
-  modalClose.addEventListener('click', () => {
-    modal.classList.remove('active');
-  });
-});
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function testWebP(callback) {
+  function testWebP(callback) {
 
     var webP = new Image();
     webP.onload = webP.onerror = function () {
@@ -53,3 +40,6 @@ testWebP(function (support) {
         document.querySelector('body').classList.add('no-webp');
     }
 });;
+
+});
+
